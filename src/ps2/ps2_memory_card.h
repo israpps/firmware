0970e54a4f3eadcf1500e967c8bfde3f8fb3eb83
Matías Israelson <57065102+israpps@users.noreply.github.com>
2023-08-11 21:51:42 -0300

    fix compilation?

--- src/ps2/ps2_memory_card.h
+++ src/ps2/ps2_memory_card.h
@@ -6,7 +6,3 @@ void ps2_memory_card_main(void);
 void ps2_memory_card_enter(void);
 void ps2_memory_card_enter_flash(void);
 void ps2_memory_card_exit(void);
-
-#define SD2PSXMAN_PROTOCOL_VER 0x001 /// TODO: Discuss if this will be an arbitrary number. or if we should use the SD2PSXMAN IRX Version number as protocol Version too
-#define SD2PSXMAN_PRODUCTS_VER 0x001
-#define IRX_VER(major, minor)	((((major) & 0xff) << 8) + ((minor) & 0xff))
