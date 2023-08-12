6643575d0f8378d4a63e456e209a5ce7cdef31d4
Matías Israelson <57065102+israpps@users.noreply.github.com>
2023-08-11 21:39:17 -0300

    enough for today?

--- src/ps2/ps2_cardman.h
+++ src/ps2/ps2_cardman.h
@@ -15,6 +15,9 @@ void ps2_cardman_open(void);
 void ps2_cardman_close(void);
 int ps2_cardman_get_idx(void);
 int ps2_cardman_get_channel(void);
+void ps2_cardman_set_idx(int T);
+void ps2_cardman_set_channel(int T);
+
 uint32_t ps2_cardman_get_card_size(void);
 
 void ps2_cardman_next_channel(void);
